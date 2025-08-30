b6164af87e3722aa07cf29f93eac64c3ff4cdb1b
LuisaPayano <luipayanomendez@crackthecode.la>
2025-08-29 19:33:12 -0500

    objeto de persona

---
+++ s30/assets/js/script.js
@@ -0,0 +1,20 @@
+console.log("Hola, esta es la sesión 30");
+//Crea un objeto de persona
+let person = {
+    name: "Luisa",
+    age: 17,
+    nationality: "Peruana",
+    birthDate: "10-03-2008",
+    hobbies: ["leer", "aprender idiomas", "preparar postres"],
+    cellphone: "987654321",
+}
+console.log(person);
+//RETO
+console.log(person.name);
+console.log(person["age"]);
+console.log(person.nationality);
+console.log(person["birthDate"]);
+console.log(person.hobbies [0]);
+console.log(person.hobbies [1]);
+console.log(person.hobbies [2]);
+console.log(person["cellphone"]);   
